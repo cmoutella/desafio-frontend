ada2faf1ec7eb76f7ea29c12ef28eb7149c87738
Carolina Moutella <ola@carolinamoutella.com>
2018-12-07 22:32:48 -0200

    CSS finalizado

--- challenge/webpack.config.js
+++ challenge/webpack.config.js
@@ -1,13 +1,15 @@
+const path = require("path");
+
 module.exports = {
-    entry: "./source/js/main.js",
-    output: {
-        filename: "bundle.js",
-        path: "./assets/js/bundle.js"
-    },
-    module: {
-        rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
-    },
-    resolve: {
-        extensions: [".js", ".jsx"]
-    }
-}
+	entry: "./src/js/main.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "assets", "js")
+	},
+	module: {
+		rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
+	},
+	resolve: {
+		extensions: [".js", ".jsx"]
+	}
+};
