45b81e99c6d04a673c53d42b143c9c1c30af8799
Carolina Moutella <ola@carolinamoutella.com>
2018-12-09 12:12:31 -0200

    Correção JS
    - Metodo unico para buscar repositorios do usuario e favoritos
    
    Correção CSS
    
    - alinhamento da lista dentro do card

--- challenge/assets/js/main.js
+++ challenge/assets/js/main.js
@@ -12,12 +12,12 @@ function append(parent, el) {
 }
 
 //Get the list of user's repositories
-function getRepositories(url) {
+function getRepos(url) {
+	ul.innerHTML=''
 	fetch(url)
 		.then(resp => resp.json())
-		.then(function(data) {
-			let repos = data.results
-			return repos.map(function(repo) {
+		.then(response => {
+			return response.map(function(repo) {
 				let li = createNode('li')
 				li.innerHTML = repo.name
 				append(ul, li)
@@ -26,45 +26,11 @@ function getRepositories(url) {
 		.catch(function(error) {
 			console.log(error)
 		})
-	document.querySelector('.qcx-repo').classList.toggle('qcx-repo--active')
+	document.querySelector('.qcx-repo').classList.add('qcx-repo--active')
 }
-
-//Get the list of user's favorite repositories
-function getFavorites(url) {
-	fetch(url)
-		.then(resp => resp.json())
-		.then(function(data) {
-			let faves = data.results
-			return faves.map(function(favorite) {
-				let li = createNode('li')
-				li.innerHTML = favorite.name
-				append(ul, li)
-			})
-		})
-		.catch(function(error) {
-			console.log(error)
-		})
-	document.querySelector('.qcx-repo').classList.toggle('qcx-repo--active')
-}
-
-//Button to display list of user's repositories
-document
-	.getElementById('btn_repositories')
-	.addEventListener('click', () =>
-		getRepositories(
-			'https://api.github.com/users/cmoutella/repos?&per_page=100'
-		)
-	)
-
-//Button to display list of user's favorite repositories
-document
-	.getElementById('btn_favorites')
-	.addEventListener('click', () =>
-		getFavorites('https://api.github.com/users/cmoutella/starred?&per_page=100')
-	)
 
 //Show User's Data
-export const fetchData = (url) => {
+const fetchData = (url) => {
 	let userData;
 	fetch(url)
 	.then(resp => resp.json())
